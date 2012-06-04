1ccf1f191d01c491af88e689b2fa34d4a4c2489b
mark johnson <markjohnson@Marks-MacBook-Pro.local>
2012-06-04 01:48:54 -0400

    adding js files

--- johnson_mark_project2/main.js
+++ johnson_mark_project2/main.js
@@ -9,10 +9,24 @@ window.addEventListener("DOMContentLoaded", function (){
 		return theElement;
 	} // end theElement function
 
+	function makeCats () {
+		var formTag = document.getElementsByTagName("form"), // form tag is an array
+			selectListItem = $('select'),
+			makeSelect = document.createElement('select') 
+			makeSelect.setAttribute("id", "groups")
+		for(var i=0, j=notesCategories; i<j; i++) {
+			var makeOption = document.createElement('option')
+			var optionText = notesCategories[i]
+			makeOption.setAttribute("value", "optionText")
+			makeOption.innerHTML = optionText
+			makeOption.appendChild(makeOption)
+		} // end for loop
+		selectListItem.appendChild(makeSelect)
+	} // end makeCat function
 
 	// Variables defaults 
 	var notesCategories = ["--Choose a Category--","Grocery","Fitness","Entertainment","Dining","Shopping","Sports"]
-
+	makeCats()
 
 
 	// Set links and submits click events 
