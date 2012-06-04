3f58a97b89eb883da5a58d253edfedda084efd74
mark johnson <markjohnson@Marks-MacBook-Pro.local>
2012-06-04 00:53:39 -0400

    added a few files and made some changes

--- johnson_mark_project2/main.js
+++ johnson_mark_project2/main.js
@@ -1,1 +1,28 @@
 // alert("am working") 
+
+// Wait until the DOM is ready 
+window.addEventListener("DOMContentLoaded", function (){
+
+	// getElementById function
+	function $(x) {
+		var theElement = document.getElementById(x)
+		return theElement;
+	} // end theElement function
+
+
+	// Variables defaults 
+	var notesCategories = ["--Choose a Category--","Grocery","Fitness","Entertainment","Dining","Shopping","Sports"]
+
+
+
+	// Set links and submits click events 
+	var displayLink = $('displayLink')
+	displayLink.addEventListener("click", getNotes)
+	var clearLink = $('clear')
+	clearLink.addEventListener("click", clearNotes)
+	var save = $('submit')
+	save.addEventListener("click", saveNotes)
+
+
+})
+
