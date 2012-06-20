0ec67432b3badeb5d3c6499ced54bc7e549b1be0
mark johnson <markjohnson@Marks-MacBook-Pro.local>
2012-06-20 12:24:35 -0400

    finish json object

---
+++ johnson_mark_project4/json.js
@@ -0,0 +1,29 @@
+var json = {
+	"note1": = {
+		"group": ["Groups:", "Entertainment"],
+		"notetitle": ["Title:","Go See a Movie This Weekend"],
+		"noteinfo": ["Note:","Me and my girlfriend will double date this weekend. Dinner and movie."],
+		"date": ["Date:","June 16, 2012"],
+		"items": ["Number of Items:","4"],
+		"attach": ["Attach a File:",""],
+		"favorite": ["Favorite Note:","Yes"]
+	},
+	"note2": = {
+		"group": ["Groups:", "Grocery"],
+		"notetitle": ["Title:","Grocery Shopping"],
+		"noteinfo": ["Note:","I have to pick up a few items at walmart on thursday."],
+		"date": ["Date:","June 14, 2012"],
+		"items": ["Number of Items:","19"],
+		"attach": ["Attach a File:",""],
+		"favorite": ["Favorite Note:","Yes"]
+	},
+	"note3": = {
+		"group": ["Groups:", "Shopping"],
+		"notetitle": ["Title:","Clothes Shopping"],
+		"noteinfo": ["Note:","I have to buy a fresh outfit for the club this weekend."],
+		"date": ["Date:","June 17, 2012"],
+		"items": ["Number of Items:","5"],
+		"attach": ["Attach a File:",""],
+		"favorite": ["Favorite Note:","Yes"]
+	}
+}
